e87a6d8c6c3ca3308e06c57c8df2c49334065c24
nklapste <nklapste@ualberta.ca>
2017-03-08 18:48:29 -0700

    Clean up comments in SWITCH_BOX.ino

--- SWITCH_BOX/.ino.cpp
+++ SWITCH_BOX/.ino.cpp
@@ -2,13 +2,13 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-03-08 18:27:21
+//This file has been generated on 2017-03-08 18:47:53
 
 #include "Arduino.h"
 #include "Arduino.h"
 void releaseKey() ;
+bool check_Switch(int Switch_ID, bool Switch_VAL, uint8_t Switch_command) ;
 void setup() ;
-bool check_Switch(int Switch_ID,bool Switch_VAL, uint8_t Switch_command);
 void loop() ;
 
 #include "SWITCH_BOX.ino"
