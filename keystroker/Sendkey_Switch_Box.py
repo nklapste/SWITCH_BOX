5915bbcee784188338f1d3347f10b8b9e2b6def6
nklapste <nklapste@ualberta.ca>
2017-04-20 18:15:50 -0600

    cleaned up terminal output

--- keystroker/Sendkey_Switch_Box.py
+++ keystroker/Sendkey_Switch_Box.py
@@ -7,27 +7,35 @@ import time
 def start_setup():
     print("\nSTARTING ARDUINO SERIAL KEYSENDER V 1.0\n")
     print("Input additional port options as needed.\
-    \nLeave blank if you want to run default.")
+    \nLeave blank if you want to run default.\n")
 
     print("PORT:", end=" ")
     port = input()
-
     if port == "":
         port = 'COM3'
 
     print("BAUDRATE:", end=" ")
     baudrate = input()
-
     if baudrate == "":
         baudrate = 9600
     else:
         baudrate = int(baudrate)
+
+    print("TIMEOUT:", end=" ")
+    timeout_val = input()
+    if timeout_val == "":
+        timeout_val = 0.5
+    else:
+        timeout_val = float(timeout_val)
+
     print("\nSelected port settings:")
     print("PORT:", port)
     print("BAUDRATE:", baudrate)
+    print("TIMEOUT:", timeout_val)
+    print("")
 
     ser = serial.Serial(port, baudrate)
-
+    ser.timeout = timeout_val
     return ser
 
 
@@ -35,7 +43,7 @@ def sendkey_char(char_in=None):
     if char_in is None:
         print("ERROR: NO INPUT")
         return
-
+    print("RECIEVED:", char_in)
     SendKeys.SendKeys(char_in)
     time.sleep(0.2)
 
@@ -58,9 +66,6 @@ def serial_read_switch(ser, mapped_keys=None):
                 mssg.append(chr(line_p))
 
             if not de_char is None:
-
-                print("RECIEVED:", de_char)
-
                 if de_char == "EXIT":
                     print("QUITING SIGNAL SENT: EXITING")
                     return
@@ -71,6 +76,7 @@ def serial_read_switch(ser, mapped_keys=None):
                     print("ERROR: INCORRECT KEY SENT")
 
                 de_char = None
+
 
 if __name__ == '__main__':
     ser = start_setup()
